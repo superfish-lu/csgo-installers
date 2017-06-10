58c8aef50ce0d9ecbe4dce058b1a45ebc2c406b1
superfish-lu <superfish-lu@users.noreply.github.com>
2017-06-10 22:48:14 +0200

    Update csgo-get5.sh

--- csgo-get5.sh
+++ csgo-get5.sh
@@ -3,17 +3,17 @@
 ##Install Get5##
 #Base Plugin and API 
 cd /home/csgoserver/serverfiles/csgo
-sudo -i -u csgoserver wget https://github.com/splewis/get5/releases/download/0.5.2/get5_0.5.2.zip
-sudo -i -u csgoserver unzip get5_0.5.2.zip
-sudo -i -u csgoserver mv addons/sourcemod/plugins/disabled/get5_apistats.smx addons/sourcemod/plugins/
+sudo -u csgoserver wget https://github.com/splewis/get5/releases/download/0.5.2/get5_0.5.2.zip
+sudo -u csgoserver unzip get5_0.5.2.zip
+sudo -u csgoserver mv addons/sourcemod/plugins/disabled/get5_apistats.smx addons/sourcemod/plugins/
 
 #SMJansson
-sudo -i -u csgoserver wget https://github.com/thraaawn/SMJansson/raw/master/bin/smjansson.ext.so
-sudo -i -u csgoserver mv smjansson.ext.so addons/sourcemod/extensions/
+sudo -u csgoserver wget https://github.com/thraaawn/SMJansson/raw/master/bin/smjansson.ext.so
+sudo -u csgoserver mv smjansson.ext.so addons/sourcemod/extensions/
 
 #SteamWorks
-sudo -i -u csgoserver wget http://users.alliedmods.net/~kyles/builds/SteamWorks/SteamWorks-git121-linux.tar.gz
-sudo -i -u csgoserver tar -xzf SteamWorks-git121-linux.tar.gz
+sudo -u csgoserver wget http://users.alliedmods.net/~kyles/builds/SteamWorks/SteamWorks-git121-linux.tar.gz
+sudo -u csgoserver tar -xzf SteamWorks-git121-linux.tar.gz
 
 ##Re-Start Server##
 cd /home/csgoserver

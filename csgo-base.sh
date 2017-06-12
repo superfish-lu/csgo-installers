57439bae2f9c1f3686bef0df9863c7e50e8bf235
superfish-lu <superfish-lu@users.noreply.github.com>
2017-06-12 15:29:58 +0200

    Create csgo-base.sh

--- csgo-base.sh
+++ csgo-base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##Disable IPv6 to avoid apt get stuck
+##Disable IPv6 to avoid apt get stuck##
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf

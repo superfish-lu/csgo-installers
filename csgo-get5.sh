9e90e4c9eed0f02f28232124a1f0f04c19b6ee18
superfish-lu <superfish-lu@users.noreply.github.com>
2017-06-17 22:25:00 +0200

    Update csgo-get5.sh

--- csgo-get5.sh
+++ csgo-get5.sh
@@ -1,36 +1,57 @@
 #!/bin/bash
+##Disable IPv6 to avoid apt get stuck##
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+sudo sysctl -p
 
-##Install Get5##
-#Base Plugin and API 
-cd /home/csgoserver/serverfiles/csgo
-sudo -u csgoserver wget https://github.com/splewis/get5/releases/download/0.5.2/get5_0.5.2.zip
-sudo -u csgoserver unzip get5_0.5.2.zip
-sudo -u csgoserver mv addons/sourcemod/plugins/disabled/get5_apistats.smx addons/sourcemod/plugins/
+##Update Ubuntu##
+#export DEBIAN_FRONTEND=noninteractive
+#apt-get update
+#apt-get upgrade -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" 
 
-#SMJansson
-sudo -u csgoserver wget https://github.com/thraaawn/SMJansson/raw/master/bin/smjansson.ext.so
-sudo -u csgoserver mv smjansson.ext.so addons/sourcemod/extensions/
+##Install Requirements##
+dpkg --add-architecture i386
+apt-get update
+apt-get -q -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" install unzip tmux lib32gcc1 libstdc++6:i386 binutils aria2
 
-#SteamWorks
-sudo -u csgoserver wget http://users.alliedmods.net/~kyles/builds/SteamWorks/SteamWorks-git121-linux.tar.gz
-sudo -u csgoserver tar -xzf SteamWorks-git121-linux.tar.gz
+##Add user csgoserver with no password##
+adduser --disabled-password --gecos "" csgoserver
 
-##Re-Start Server##
+##Set password for user csgoserver##
+yes gocs1337 | passwd csgoserver
+
+##Preload csgo files from files.e-sports.lu for faster install
 cd /home/csgoserver
-sudo -i -u csgoserver ./csgoserver stop
-sudo -i -u csgoserver ./csgoserver start
+sudo -i -u csgoserver aria2c http://files.e-sports.lu/serverfiles.tar.gz http://retake.e-sports.lu/serverfiles.tar.gz http://multi.e-sports.lu/serverfiles.tar.gz
+sudo -i -u csgoserver tar -xzvf serverfiles.tar.gz
+sudo -i -u csgoserver rm serverfiles.tar.gz
 
-##Record % Store Server Details##
-#Full output to tmp.txt then grep relevant details to srv-details-color.txt
-sudo -i -u csgoserver ./csgoserver details > tmp.txt
-sudo -i -u csgoserver grep "Server name" tmp.txt >> srv-details-colors.txt
-sudo -i -u csgoserver grep "Server IP" tmp.txt >> srv-details-colors.txt
-sudo -i -u csgoserver grep "Server password" tmp.txt >> srv-details-colors.txt
-sudo -i -u csgoserver grep "RCON password" tmp.txt >> srv-details-colors.txt
-sudo -i -u csgoserver grep "Maxplayers" tmp.txt >> srv-details-colors.txt
-sudo -i -u csgoserver grep "Game mode" tmp.txt >> srv-details-colors.txt
-sudo -i -u csgoserver grep "Tick rate" tmp.txt >> srv-details-colors.txt
-sudo -i -u csgoserver grep "Status" tmp.txt >> srv-details-colors.txt
+##Get csgoserver installer from web and launch it##
+sudo -i -u csgoserver wget https://gameservermanagers.com/dl/csgoserver
+sudo -i -u csgoserver chmod +x csgoserver
+sudo -i -u csgoserver ./csgoserver auto-install
 
-#Remove Colors Code from Output for Readability
-sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" srv-details-colors.txt > srv-details.txt
+##Install metamod and sourcemod##
+#sudo -i -u csgoserver printf 'metamod' | sudo -i -u csgoserver ./csgoserver mods-install
+#sudo -i -u csgoserver printf 'sourcemod' | sudo -i -u csgoserver ./csgoserver mods-install
+
+
+##Change Config##
+#Hostname
+sudo -i -u csgoserver sed -i 's/hostname "LinuxGSM"/hostname "E-Sports.lu - Match #1337"/' /home/csgoserver/serverfiles/csgo/cfg/csgo-server.cfg
+#GSLT
+sudo -i -u csgoserver sed -i 's/gslt=""/gslt="5BB4C57E5A5A45F60126EAE8D15021C1"/' /home/csgoserver/csgoserver
+#Game Mode
+sudo -i -u csgoserver sed -i 's/gamemode="0"/gamemode="1"/' /home/csgoserver/csgoserver
+
+#Tick Rate
+sudo -i -u csgoserver sed -i 's/tickrate="64"/tickrate="128"/' /home/csgoserver/csgoserver
+
+#Enable HLTV for Demo Recording
+sudo -i -u csgoserver echo "tv_enable 1" >> /home/csgoserver/serverfiles/csgo/cfg/csgo-server.cfg
+
+#Add admins to SourceMod
+#sudo -i -u csgoserver echo '"STEAM_0:0:77074401" "99:z"' >> /home/csgoserver/serverfiles/csgo/addons/sourcemod/configs/admins_simple.ini
+#sudo -i -u csgoserver echo '"STEAM_0:1:18037936" "99:z"' >> /home/csgoserver/serverfiles/csgo/addons/sourcemod/configs/admins_simple.ini
+#sudo -i -u csgoserver echo '"STEAM_0:1:2483865" "99:z"' >> /home/csgoserver/serverfiles/csgo/addons/sourcemod/configs/admins_simple.ini
